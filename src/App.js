f4c2318b8a1e9b080ef35526df44b6918379b6cf
alfonz <aalonso159@gmail.com>
2021-08-05 22:21:35 +1000

    my first upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Card from './components/Card';
+import Board from './components/Board';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main className="flexbox">
+        <Board id="board-1" className="board">
+          <Card id="card-1" className="card" draggable="true">
+            <p>Card 1</p>
+          </Card>
+        </Board>
+        <Board id="board-2" className="board">
+          <Card id="card-2" className="card" draggable="true">
+            <p>Card 2</p>
+          </Card>
+        </Board>
+      </main>
+    </div>
+  );
+}
+
+export default App;
